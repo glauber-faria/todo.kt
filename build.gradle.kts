3738f069bb4ef29af549db94b5c1e777c24f8158
Glauber de Souza Faria <glauber.faria@pti.org.br>
2022-05-22 19:47:30 -0300

    update project

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.0"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.sonarqube") version "3.3"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
@@ -10,6 +11,7 @@ plugins {
 group = "com.studies"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+
 
 repositories {
 	mavenCentral()
@@ -35,3 +37,13 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+sonarqube{
+	properties {
+		property ("sonar.projectKey", "glauber-faria_todo.kt")
+		property ("sonar.organization", "glauber-faria")
+		property ("sonar.host.url", "https://sonarcloud.io")
+	}
+}
+
+
